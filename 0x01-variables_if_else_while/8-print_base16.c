a1e31fc4b474a08b169ce3761955f201260c4671
kidaholy <kidayos2014@gmail.com>
2023-06-16 09:12:41 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*description:- print all the digits
+* Return:0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+	putchar (x);
+	for (i = 'a'; i <= 'f'; ++i)
+	putchar (i);
+
+	putchar('\n');
+
+	return (0);
+}
