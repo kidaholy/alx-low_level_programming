6da61ba4f151af6f4c1a0e920470cf5b6b672436
kidaholy <kidayos2014@gmail.com>
2023-06-16 10:02:56 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,19 @@
  * Description: Get a random number and check its last digit, compare it with 5
  * Return: 0
  */
-int main(void)
-{
-	int num = 0;
+ int main(void)
+ {
+     int num = 0;
      
-     	while(num <= 10)
-     	{
-        	putchar(48 + num);
-        	if(num != 9)
-         	{
-             		putchar(',');
-             		putchar(' ');
-         	}
-         	num++;
-     	}
-     	return(0);
-
+     while (num <= 10)
+     {
+         putchar(48 + num);
+         if (num != 9)
+         {
+             putchar(',');
+             putchar(' ');
+         }
+         num++;
+     }
+     return (0);
+ }
