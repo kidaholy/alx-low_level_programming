78712d67f78d0bf3a3b41619468e066d5f79ba6a
kidaholy <kidayos2014@gmail.com>
2023-06-16 08:43:52 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+    int a = 0;
+    
+    while(a <= 9)
+    {
+        putchar(48 + a);
+        a++;
+    }
+    
+	putchar('\n');
+	return (0);
+}
