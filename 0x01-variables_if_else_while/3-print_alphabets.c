3669555e8de274c3325e23d41894f4ea57485c01
kidaholy <kidayos2014@gmail.com>
2023-06-16 06:21:40 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	for(char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for(char i = 'A'; i<= 'Z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
