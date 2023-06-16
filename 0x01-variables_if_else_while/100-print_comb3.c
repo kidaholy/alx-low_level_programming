c03787de35c4fec1b467478d7689144d58448fdd
kidaholy <kidayos2014@gmail.com>
2023-06-16 10:22:01 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	while (b < 10)
+	{
+		a = 0;
+		while (a < 10)
+		{
+			if (b != a && b < a)
+			{
+				putchar('0' + b);
+				putchar('0' + a);
+
+				if (a + b != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			a++;
+		}
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
