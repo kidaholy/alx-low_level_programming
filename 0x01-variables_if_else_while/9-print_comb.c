a1729358702710bc24982e51725da9babe61c887
kidaholy <kidayos2014@gmail.com>
2023-06-16 10:08:32 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
  * Return: 0
  */
- int main(void)
- {
-     int num = 0;
-     
-     while (num <= 10)
-     {
-         putchar(48 + num);
-         if (num != 9)
-         {
-             putchar(',');
-             putchar(' ');
-         }
-         num++;
-     }
-     return (0);
- }
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}}
