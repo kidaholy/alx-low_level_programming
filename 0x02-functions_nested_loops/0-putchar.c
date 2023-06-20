10dd6fa3ef3ffd8dc154cca70a8c0b316090c242
kidaholy <kidayos2014@gmail.com>
2023-06-20 03:42:02 -0700

    main

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+/**
+ *main-main block
+ *Description:print -putchar in a new line
+ *Return:this program returns zero
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
