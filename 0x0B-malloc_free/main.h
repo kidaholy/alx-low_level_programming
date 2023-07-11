cb38699669514ffb1f1a68e6813ac161669b0d17
kidaholy <kidayos2014@gmail.com>
2023-07-10 17:29:09 -0700

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,21 +1,13 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-#include <stdio.h>
-#include <stdlib.h>
-
-char *_strdup(char *str);
-
-char **strtow(char *str);
-
-char *argstostr(int ac, char **av);
-
-char *str_concat(char *s1, char *s2);
-
-void free_grid(int **grid, int height);
-
-int **alloc_grid(int width, int height);
+#ifndef MAIN_H
+#define MAIN_H
 
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
 #endif
+
