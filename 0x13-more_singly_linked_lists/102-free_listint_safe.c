6b0f8af4dbe48eb0ceeffbc452440281bec0eab0
kidaholy <kidayos2014@gmail.com>
2023-08-01 12:16:58 -0700

    task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,13 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
-
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
  *
  * Return: number of elements in the freed list
- **/
-
+ */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
